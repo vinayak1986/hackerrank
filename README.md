e7e57f18e628df4d065caeeb7f766ae2d951fb1c
vinayak1986 <vinayak1986@gmail.com>
2019-03-19 07:03:28 +0530

    Modified bitwise_and.py to fix previous issues and edited README.md to reflect new code

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ The first commit has the initial versions of bitwise_and.py, second_lowest.py an
 
 March 19, 2019
 Modified bitwise_and.py to fix the code timeout issue with the previous version.
+
+March 19, 2019
+Modified bitwise_and.py to fix the previous issues.
--- bitwise_and.py
+++ bitwise_and.py
@@ -1,7 +1,10 @@
-# The first version of the code caused code timeout issues. This is an attempt to fix
-# it by looping through a in reverse order - n-1 to 1. We exit the loop at the first
-# encounter of a number satisfying the condition - a & b < k. If nothing was found,
-# we add 0 to the results list.
+# The previous version of the code was fixed to address the code timeout issue in the first version.
+# It ended up failing basic test cases on hackerrank. This version combines the approaches from
+# the previous versions - looping through a in reverse and exiting the loop on a condition.
+# The maximum possible value of k less than k is k - 1 and if we see this value in any a & b
+# evaluation, we exit the inner loops and add the value to the final results list. If it is
+# not encountered, we find the maximum of the other 'valid' (a & b less than k) values.
+# This version passed all basic and hidden test cases on hackerrank and is the final submission.
 if __name__ == '__main__':
 	t = int(input())
 	results = []
@@ -13,12 +16,14 @@ if __name__ == '__main__':
 		valid_a_and_b = []
 		for a in range(n - 1, 0, -1):
 			for b in range(a + 1, n + 1):
-				if ((a & b) < k) and ((a & b) > 0):
+				if (a & b) == k - 1:
 					results.append(str(a & b))
 					found = 1
 					break
+				if (a & b) < k:
+					valid_a_and_b.append((a & b))
 			if found == 1:
 				break
 		if not found:
-			results.append(str(0))
+			results.append(str(max(valid_a_and_b)))
 	print('\n'.join(results))
