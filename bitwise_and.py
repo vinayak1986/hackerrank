accdd221dd45d4ccc0f7a931ea19fad7a2120c97
vinayak1986 <vinayak1986@gmail.com>
2019-03-19 06:50:49 +0530

    Modified bitwise_and.py to fix the code timeout issue with the previous version

--- bitwise_and.py
+++ bitwise_and.py
@@ -1,19 +1,24 @@
-# We loop through all possible combinations of a & b from 1 to n -1 and a + 1 to n respectively. 
-# All valid values matching the required condition (a & b < k) are added to a running list.
-# At the end of the two inner loops, the maximum value of the running list is added to a
-# results list.
+# The first version of the code caused code timeout issues. This is an attempt to fix
+# it by looping through a in reverse order - n-1 to 1. We exit the loop at the first
+# encounter of a number satisfying the condition - a & b < k. If nothing was found,
+# we add 0 to the results list.
 if __name__ == '__main__':
 	t = int(input())
 	results = []
 	for t_itr in range(t):
+		found = 0
 		nk = input().split()
 		n = int(nk[0])
 		k = int(nk[1])
 		valid_a_and_b = []
-		for a in range(1, n):
+		for a in range(n - 1, 0, -1):
 			for b in range(a + 1, n + 1):
-				if (a & b) < k:
-					valid_a_and_b.append(a & b)
-		results.append(str(max(valid_a_and_b)))
+				if ((a & b) < k) and ((a & b) > 0):
+					results.append(str(a & b))
+					found = 1
+					break
+			if found == 1:
+				break
+		if not found:
+			results.append(str(0))
 	print('\n'.join(results))
-
